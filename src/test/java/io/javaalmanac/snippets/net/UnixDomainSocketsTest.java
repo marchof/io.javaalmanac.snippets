29b9e03bc51356da01cbffcc664f9d85df76a57b
Marc R. Hoffmann <hoffmann@mountainminds.com>
2022-03-23 16:51:10 +0100

    Simplify example code

--- src/test/java/io/javaalmanac/snippets/net/UnixDomainSocketsTest.java
+++ src/test/java/io/javaalmanac/snippets/net/UnixDomainSocketsTest.java
@@ -1,5 +1,8 @@
 package io.javaalmanac.snippets.net;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import io.javaalmanac.snippets.ConsoleGrabber;
@@ -8,9 +11,12 @@ public class UnixDomainSocketsTest {
 
 	@Test
 	void run_main() throws Exception {
-		ConsoleGrabber.assertOutEquals(UnixDomainSockets::main, """
-				Hello Socket!
-				""");
+		var grabber = new ConsoleGrabber();
+		grabber.run(UnixDomainSockets::main);
+		var lines = grabber.lines();
+		assertEquals(2, lines.size());
+		assertTrue(lines.get(0).contains("Listening on"));
+		assertEquals("Hello Socket!", lines.get(1));
 	}
 
 }
