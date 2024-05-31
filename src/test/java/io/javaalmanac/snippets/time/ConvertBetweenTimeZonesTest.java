9bbbcc6449e39a1d86b3ef0588ec8ba8fce19a92
Marc R. Hoffmann <hoffmann@mountainminds.com>
2024-05-31 21:12:07 +0200

    Use future dates

--- src/test/java/io/javaalmanac/snippets/time/ConvertBetweenTimeZonesTest.java
+++ src/test/java/io/javaalmanac/snippets/time/ConvertBetweenTimeZonesTest.java
@@ -9,11 +9,11 @@ public class ConvertBetweenTimeZonesTest {
 	@Test
 	void run_main() throws Exception {
 		ConsoleGrabber.assertOutEquals(ConvertBetweenTimeZones::main, """
-				Lunar Eclipse at 2022-11-08T19:59+09:00[Asia/Tokyo]
-				Lunar Eclipse at 2022-11-08T16:29+05:30[Asia/Kolkata]
-				Lunar Eclipse at 2022-11-08T11:59+01:00[Europe/Paris]
-				Lunar Eclipse at 2022-11-08T07:59-03:00[America/Fortaleza]
-				Lunar Eclipse at 2022-11-08T00:59-10:00[Pacific/Honolulu]
+				Lunar Eclipse at 2025-03-14T15:59+09:00[Asia/Tokyo]
+				Lunar Eclipse at 2025-03-14T12:29+05:30[Asia/Kolkata]
+				Lunar Eclipse at 2025-03-14T07:59+01:00[Europe/Paris]
+				Lunar Eclipse at 2025-03-14T03:59-03:00[America/Fortaleza]
+				Lunar Eclipse at 2025-03-13T20:59-10:00[Pacific/Honolulu]
 				""");
 	}
 
