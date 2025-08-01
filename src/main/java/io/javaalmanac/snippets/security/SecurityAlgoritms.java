8bf0fc9841d2df297a62729665e6cbd3f3175486
Marc R. Hoffmann <hoffmann@mountainminds.com>
2025-08-01 22:08:51 +0200

    Add snippet to list all cryptographic algorithms

---
+++ src/main/java/io/javaalmanac/snippets/security/SecurityAlgoritms.java
@@ -0,0 +1,44 @@
+package io.javaalmanac.snippets.security;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toCollection;
+
+import java.security.Provider.Service;
+import java.security.Security;
+import java.util.Arrays;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+/**
+ * The JDK comes with a long list of cryptographic algorithms for different
+ * {@link java.security.Provider$Service} types. This snippet lists all service
+ * types together with the algorithms implemented in the current JDK.
+ * 
+ * @title JDK Security Algorithms
+ * @category api.security
+ * @since 1.1
+ */
+public class SecurityAlgoritms {
+
+	static Stream<Service> getAllSecurityServices() {
+		return Arrays.stream(Security.getProviders()).flatMap(p -> p.getServices().stream());
+	}
+
+	public static void main(String[] args) {
+
+		var grouped = getAllSecurityServices().collect(
+				// Group algorithms by type
+				groupingBy(Service::getType,
+						// Sort types and algorithms using tree collections
+						TreeMap::new, mapping(Service::getAlgorithm, toCollection(TreeSet::new))));
+
+		grouped.forEach((type, algorithms) -> {
+			System.out.println(type);
+			algorithms.forEach(a -> System.out.println("* " + a));
+		});
+
+	}
+
+}
